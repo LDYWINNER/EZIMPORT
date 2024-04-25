b31c341babc4c9031ee1f96ef8430271c135f92d
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-04-25 15:47:48 -0400

    ADD - dash board base layout

--- ezimport-front/src/components/tab-bar.tsx
+++ ezimport-front/src/components/tab-bar.tsx
@@ -94,7 +94,7 @@ export default async function TabBar({
                 className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
               >
                 <Package className="h-4 w-4" />
-                Products{" "}
+                Products
               </Link>
               <Link
                 href="#"
