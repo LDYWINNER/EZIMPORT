77e5013449ad1fa6bc7ca0ceb402bc901cf3ae98
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-05-02 16:40:09 -0400

    CHORE

--- ezimport-front/src/lib/puppeteerSetup.ts
+++ ezimport-front/src/lib/puppeteerSetup.ts
@@ -3,7 +3,7 @@ import chromium from "@sparticuz/chromium";
 import puppeteer from "puppeteer-core";
 
 export default async function setupPuppeteer() {
-  console.log("Chromium path:", await chromium.executablePath);
+  console.log("Chromium path:", await chromium.executablePath());
 
   const browser = await puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
