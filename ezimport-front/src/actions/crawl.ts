ebeb739afe576471ea18cb84ff76b2a2af0ce06a
LDYWINNER <79012707+LDYWINNER@users.noreply.github.com>
2024-05-06 20:37:33 -0400

    UPDATE - puppeteer launch args

--- ezimport-front/src/actions/crawl.ts
+++ ezimport-front/src/actions/crawl.ts
@@ -20,7 +20,18 @@ export async function crawlAndDownload(formData: FormData) {
 
   // crawl
   // for local
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
 
   // for deploy
   // const browser = await setupPuppeteer();
